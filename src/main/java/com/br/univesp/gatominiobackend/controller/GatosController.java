759aa41c9b5a1b32a1fcda43b24358e93cddf790
Geni <51093689+GeniferGonzalezSantos@users.noreply.github.com>
2024-05-12 12:05:27 -0300

    Update GatosController.java
    adicionando CORS e nova rota para cadastro de gato  sem foto de perfil

--- src/main/java/com/br/univesp/gatominiobackend/controller/GatosController.java
+++ src/main/java/com/br/univesp/gatominiobackend/controller/GatosController.java
@@ -3,12 +3,19 @@ package com.br.univesp.gatominiobackend.controller;
 import com.br.univesp.gatominiobackend.DTO.RetornoGatoDTO;
 import com.br.univesp.gatominiobackend.entity.CatProfile;
 import com.br.univesp.gatominiobackend.service.GatosService;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
 
 @Controller
 public class GatosController {
@@ -17,22 +24,46 @@ public class GatosController {
     public GatosController(GatosService gatosService) {
         this.gatosService = gatosService;
     }
-
-    @GetMapping("/buscarGato/{idGato}")
+    @CrossOrigin(origins = "*")
+    @GetMapping("/buscarGatoComFoto/{idGato}")
     public ResponseEntity<RetornoGatoDTO> searchCatProfile(@PathVariable(value = "idGato") Long idGato) {
         var catProfile = gatosService.buscarGato(idGato);
-
+        Path path = Paths.get(catProfile.getLink_foto());
+        Resource resource = null;
+        try {
+            resource = new UrlResource(path.toUri());
+            byte[] imageBytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
+            String imageBase64 = Base64.getEncoder().encodeToString(imageBytes);
+            catProfile.setLink_foto(imageBase64);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
        return ResponseEntity.ok(catProfile);
     }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/buscarGato/{idGato}")
+    public ResponseEntity<RetornoGatoDTO> searchCat(@PathVariable(value = "idGato") Long idGato) {
+        var catProfile = gatosService.buscarGato(idGato);
+        return ResponseEntity.ok(catProfile);
+    }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/buscarGato")
     public ResponseEntity<Iterable<RetornoGatoDTO>> searchAllCatProfile() {
         var catProfile = gatosService.listarGatos();
         return ResponseEntity.ok(catProfile);
     }
 
-    @PostMapping("/salvarGato")
-    public ResponseEntity<RetornoGatoDTO> saveCatProfile(@RequestBody  CatProfile catProfile) {
+    @CrossOrigin(origins = "*")
+    @PostMapping(value = "/salvarGatoComFoto", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<RetornoGatoDTO> saveCatProfileWithImage(@RequestPart("catProfile") CatProfile catProfile, @RequestParam("image") MultipartFile image) {
+        var dto = gatosService.salvarGatoComFoto(catProfile, image);
+        return ResponseEntity.ok(dto);
+
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(value = "/salvarGato")
+    public ResponseEntity<RetornoGatoDTO> saveCatProfile(@RequestBody CatProfile catProfile) {
         var dto = gatosService.salvarGato(catProfile);
         return ResponseEntity.ok(dto);
 
